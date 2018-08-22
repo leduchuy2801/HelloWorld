e86f4a70529e5de919b1aea4d4916b84ea15f973
unknown <leduchuy2801@gmail.com>
2018-08-22 16:06:15 +0700

    phuong trinh bac 2

--- src/huy/dev/HelloWorld.java
+++ src/huy/dev/HelloWorld.java
@@ -3,5 +3,26 @@ package huy.dev;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		double a = 10;
+		double b = 8;
+		double c = 3;
+		double delta;
+		double x1, x2;
+		delta = (b*b) - (4*a*c);
+		if (delta < 0){
+			System.out.println("Phuong trinh vo nghiem");
+		}
+		
+		if (delta > 0){
+			x1 = (-b+Math.sqrt(delta)) / (2*a);
+			x2 = (-b-Math.sqrt(delta)) / (2*a);
+			System.out.println("x1 = " + x1);
+			System.out.println("x2 = " + x2);
+		}
+		
+		if (delta == 0) {
+			x1 = -b / (2*a);
+			System.out.println("Phuong trinh co nghiem kep x = " + x1);
+		}
 	}
 }
